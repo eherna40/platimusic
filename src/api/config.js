e004e5c538524984db818ee6980d3292a2557b45
Emilio Hernandez <eherna40@gmail.com>
2018-11-13 13:22:21 +0100

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 export default{
     appName: 'platzimusic',
-    apiKey: 'cba41aedc1af5f7af436395255ad91a9',
-    secret: '03e251b0be806ea8fcdb693967501fc5',
+    apiKey: '*************************',
+    secret: '************************',
     registreredTo: 'eherna'
 }
 
